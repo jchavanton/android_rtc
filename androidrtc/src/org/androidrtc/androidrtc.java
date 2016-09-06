bad543ee87f2047ac1efddd1d6c8ec50da978c9c
Julien Chavanton <jchavanton@gmail.com>
2016-09-06 12:36:18 +0200

    android project created

---
+++ androidrtc/src/org/androidrtc/androidrtc.java
@@ -0,0 +1,15 @@
+package org.androidrtc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class androidrtc extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
